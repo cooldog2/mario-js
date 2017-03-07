2748cd77320ffae30e7382512a61442b80b2aa7c
Anita <karrpd@juno.com>
2017-03-06 19:22:54 -0500

    completed assignment

--- mario.js
+++ mario.js
@@ -12,20 +12,25 @@ function determineHeightAndThenDrawPyramid() {
 
     // TODO 3
     // figure out the height the user typed (replace the "5" below)
-    heightStr = "5";
+      //heightStr = "5";
+   heightStr = document.getElementById("height").value;
+   //heightStr = document.querySelector("#heightStr");
 
     // here we convert the string to an int
     height = parseInt(heightStr);
 
     // TODO 2
     // draw the pyramid with the given height
+    drawPyramid(height);
+
 
 }
 
-
 // TODO 1
 // hook up the button's click event to our determineHeightAndThenDrawPyramid function
-
+  var button = document.querySelector("#button");
+  button.addEventListener("click", determineHeightAndThenDrawPyramid);
+  // document.getElementById("myBtn").addEventListener("click", myFunction);
 
 
 /**
@@ -37,7 +42,7 @@ function determineHeightAndThenDrawPyramid() {
 
      // TODO 4
      // before drawing, clear the old content
-
+      document.getElementById("pyramid").innerhtml = "";
 
      // for each row....
      for (var row = 0; row < height; row++) {
